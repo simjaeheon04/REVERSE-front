bbc7149f4011666b5d25f6df901f8268d85f4511
PJongHo03 <ghdls1254@gmail.com>
2026-05-01 19:26:53 +0900

    feat: 로그인 상태 관리 추가

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import Router from "../routes";
+import { useAuthStore } from "../stores/authStore";
 import GlobalStyle from "../styles/globalStyle";
 
 function App() {
+  const hydrate = useAuthStore((state) => state.hydrate);
+
+  useEffect(() => {
+    hydrate();
+  }, [hydrate]);
+
   return (
     <>
       <GlobalStyle />
