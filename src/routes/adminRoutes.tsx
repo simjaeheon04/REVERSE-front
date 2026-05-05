3bcbb1574ce969d771fc2a656209a0efb697f2b3
PJongHo03 <ghdls1254@gmail.com>
2026-05-05 09:20:04 +0900

    feat: 관리자에 따른 라우터 설정

--- src/routes/adminRoutes.tsx
+++ src/routes/adminRoutes.tsx
@@ -1,28 +1,76 @@
 import AdminPage from "../pages/admin/AdminPage";
+import CalendarManagePage from "../pages/calendarManage/CalendarManagePage";
 import ClubIntroManagePage from "../pages/clubIntroManage/ClubIntroManagePage";
+import NoticeManagePage from "../pages/noticeManage/NoticeManagePage";
 import OfficerManagePage from "../pages/officerManage/OfficerManagePage";
 import ProjectManagePage from "../pages/projectManage/ProjectManagePage";
+import RecruitManagePage from "../pages/recruitManage/RecruitManagePage";
 import TermsManagePage from "../pages/termsManage/TermsManagePage";
+import AdminRouteGuard from "./AdminRouteGuard";
 
 export const adminRoutes = [
   {
     path: "/admin",
-    element: <AdminPage />,
+    element: (
+      <AdminRouteGuard>
+        <AdminPage />
+      </AdminRouteGuard>
+    ),
   },
   {
     path: "/admin/club-intro",
-    element: <ClubIntroManagePage />,
+    element: (
+      <AdminRouteGuard>
+        <ClubIntroManagePage />
+      </AdminRouteGuard>
+    ),
   },
   {
     path: "/admin/officer",
-    element: <OfficerManagePage />,
+    element: (
+      <AdminRouteGuard>
+        <OfficerManagePage />
+      </AdminRouteGuard>
+    ),
   },
   {
     path: "/admin/terms",
-    element: <TermsManagePage />,
+    element: (
+      <AdminRouteGuard>
+        <TermsManagePage />
+      </AdminRouteGuard>
+    ),
   },
   {
     path: "/admin/project",
-    element: <ProjectManagePage />,
+    element: (
+      <AdminRouteGuard>
+        <ProjectManagePage />
+      </AdminRouteGuard>
+    ),
+  },
+  {
+    path: "/admin/calendar",
+    element: (
+      <AdminRouteGuard>
+        <CalendarManagePage />
+      </AdminRouteGuard>
+    ),
+  },
+  {
+    path: "/admin/notice",
+    element: (
+      <AdminRouteGuard>
+        <NoticeManagePage />
+      </AdminRouteGuard>
+    ),
+  },
+  {
+    path: "/admin/recruit",
+    element: (
+      <AdminRouteGuard>
+        <RecruitManagePage />
+      </AdminRouteGuard>
+    ),
   },
 ];
